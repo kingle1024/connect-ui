f5b5d44df711c6798622562c57d1e16f67537b3f
kingle1024 <teran1024@naver.com>
2025-11-27 15:43:17 +0900

    ✨ Feat(#43): [채팅] 로그인 하지 않은 상태에서 채팅 하단 탭 네비 클릭 시 알럿 표시

--- src/navigation/BottomTabNavigation.tsx
+++ src/navigation/BottomTabNavigation.tsx
@@ -1,11 +1,18 @@
+import React, { useContext } from "react";
+import Alert from '@blazejkustra/react-native-alert';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feather, Entypo } from "@expo/vector-icons";
 import ConnectNavigation from "./ConnectNavigation";
 import ChatNavigation from "./ChatNavigation";
+import AuthContext from "@/components/auth/AuthContext";
+import { useRootNavigation } from "@/hooks/useNavigation";
 
 const BottomTab = createBottomTabNavigator();
 
 const BottomTabNavigation = () => {
+  const { user: me } = useContext(AuthContext);
+  const rootNavigation = useRootNavigation(); 
+
   return (
     <BottomTab.Navigator
       screenOptions={{
@@ -38,6 +45,27 @@ const BottomTabNavigation = () => {
             <Entypo name="chat" size={20} color={focused ? "tomato" : "gray"} />
           ),
         }}
+        listeners={() => ({
+          tabPress: (e) => {
+            // 로그인 상태 확인
+            if (!me) {
+              e.preventDefault(); // <--- 이게 핵심! 탭 네비게이션을 멈춥니다.
+              Alert.alert(
+                "로그인이 필요합니다.",
+                "채팅을 이용하려면 로그인이 필요합니다.",
+                [
+                  {
+                    text: "로그인",
+                    onPress: () => {
+                      rootNavigation.navigate("Signin");
+                    },
+                  },
+                  { text: "닫기" },
+                ]
+              );
+            }
+          },
+        })}
       />
     </BottomTab.Navigator>
   );
